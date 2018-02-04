01047b56d01615fbb8bfbde04cc5a1196a5ac004
gbanm <gbanm06@gmail.com>
2018-02-04 01:41:42 +0100

    added Resource

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ExoPlanet from './src/ExoPlanet'
+
+import{resource} from './src/config/Resource'
 
 export default class App extends React.Component {
   render() {
 
-   
     return (
         <View style={styles.container}>
-           <ExoPlanet />  
+        <Text> 
+       
+</Text>
       </View>
 
     );
@@ -18,7 +20,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/config/Resource.ts
@@ -0,0 +1,10 @@
+
+import LocalizedStrings from 'react-native-localization';
+
+ export const resource = new LocalizedStrings({
+  "en-US":{
+    message:"ExoPlan",
+  
+  },
+ 
+ });
