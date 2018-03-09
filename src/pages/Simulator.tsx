ce03eae621affc38d230b551a9adb114c38ed299
gbanm <gbanm06@gmail.com>
2018-03-09 23:31:00 +0100

    simple simulator

--- src/pages/Simulator.tsx
+++ src/pages/Simulator.tsx
@@ -1,18 +1,52 @@
-import * as React from 'react';
-import {AppRegistry, StyleSheet, View } from 'react-native';
-import { Container } from 'native-base';
-import{resource} from '../config/Resource'
-import styles from '../styles/defaultStyle'
-
-export default class Simulator extends React.Component {
-
-  render() {
+import  * as React from "react";
+import { AppRegistry, StyleSheet, Dimensions, View } from "react-native";
+import { GameLoop } from "react-native-game-engine";
  
-    return (
-      <Container style={styles.d3View} >
-      
-      </Container>
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+const RADIUS = 25;
+ 
+export default class Simulator extends React.PureComponent<any,{x:number,y:number}> {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      x: WIDTH / 2 - RADIUS,
+      y: HEIGHT / 2 - RADIUS
+    };
+  }
+ 
+  updateHandler = ({ touches, screen, time }) => {
+    let move = touches.find(x => x.type === "move");
+    if (move) {
+      this.setState({
+        x: this.state.x + move.delta.pageX,
+        y: this.state.y + move.delta.pageY
+      });
+    }
+  };
+ 
+  render() {
+    return (
+      <GameLoop style={styles.container} onUpdate={this.updateHandler}>
+ 
+        <View style={[styles.player, { left: this.state.x, top: this.state.y }]} />
+ 
+      </GameLoop>
     );
   }
 }
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+  player: {
+    position: "absolute",
+    backgroundColor: "pink",
+    width: RADIUS * 2,
+    height: RADIUS * 2,
+    borderRadius: RADIUS * 2
+  }
+});
+ 
