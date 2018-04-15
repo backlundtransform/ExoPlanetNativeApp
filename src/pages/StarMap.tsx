d07cdf236d7b7caa798d6a26f61d6be87e1d9f9d
gbanm <gbanm06@gmail.com>
2018-04-15 13:54:12 +0200

    dont need sensor manager

--- src/pages/StarMap.tsx
+++ src/pages/StarMap.tsx
@@ -13,14 +13,19 @@ import { decorator as sensors } from "react-native-sensors";
 import{siderealtime,dot_product,right_ascension,getdeclination } from '../sensor/mathfunctions'
 import Compass from '../sensor/compass'
 import RNSimpleCompass from 'react-native-simple-compass';
+import {
+  DeviceEventEmitter, NativeModules
+} from 'react-native';
+
+
 
 import styles from "../styles/defaultStyle"
 
 const start =  {
   latitude: 51,
   longitude: 0,
-  latitudeDelta: 5,
-  longitudeDelta: 5,
+  latitudeDelta: 20,
+  longitudeDelta: 10,
 } 
 interface StarmapProp { navigation:any, Accelerometer:any, gyroscop:any}
 interface StarmapState {region: any,
@@ -45,7 +50,7 @@ interface StarmapState {region: any,
      altitude:0,
      siderealtime:""
     };
-   
+  
   }
 
  success=(pos)=> {
@@ -76,7 +81,7 @@ onRegionChange(region) {
     const rightascension = 12 + -1*region.longitude/15
     const declination =region.latitude
     const zoom =Math.log2(360 * ((width/256) / region.longitudeDelta)) + 1
-
+console.log(zoom)
     if(this.refs.map && !(this.state.gps)){
 
     this.setState({rightascension,declination})
@@ -89,9 +94,11 @@ onRegionChange(region) {
        }
 
        componentWillReceiveProps(nextProps){
+
+     
+ 
         let {region,currentRegion,altitude, degree,rightascension,declination}= this.state
-  
-    
+ 
         const gps = nextProps.navigation.state.params&&nextProps.navigation.state.params.gps
         if(this.state.currentRegion!== undefined && (gps!==undefined &&gps)){
          const rightascension =right_ascension(currentRegion.longitude,currentRegion.latitude,altitude, degree)
@@ -100,27 +107,29 @@ onRegionChange(region) {
         
 
       const longitude = (rightascension-12)<180?(rightascension-12):(rightascension-12)-360;
-      if(region.longitude+10<longitude ||region.longitude-10>longitude){
+      if(region.longitude+2<longitude ||region.longitude-2>longitude){
       region = {
         latitude: declination,
         longitude: longitude,
-        latitudeDelta: 7,
-        longitudeDelta: 7,
+        latitudeDelta: 10,
+        longitudeDelta: 10,
       } 
-      this.setState({ gps, region,rightascension,declination, altitude :dot_product(nextProps.Accelerometer.z,nextProps.Accelerometer.y,nextProps.Accelerometer.x,1,0,0), siderealtime:siderealtime(this.state.currentRegion.longitude)});
+const altitude =dot_product(nextProps.Accelerometer.z,nextProps.Accelerometer.y,nextProps.Accelerometer.x,1,0,0)
+     
+      this.setState({ gps, region,rightascension,declination, altitude, siderealtime:siderealtime(this.state.currentRegion.longitude) });
  
     }
   
- 
+
+    const degree_update_rate = 3;
+    RNSimpleCompass.start(degree_update_rate, (degree) => {
+    
+      if(this.refs.map && (degree+2<this.state.degree ||degree-2>this.state.degree)){
        
-        const degree_update_rate = 3;
-        RNSimpleCompass.start(degree_update_rate, (degree) => {
-          if(this.refs.map && (degree+10<this.state.degree ||degree-10>this.state.degree)){
-            
-          this.setState({degree})
-          }
-          RNSimpleCompass.stop();
-        });
+    this.setState({degree})
+      }
+      RNSimpleCompass.stop();
+    });
 
       }else{
 
@@ -143,11 +152,11 @@ onRegionChange(region) {
         initialRegion={start}
         region={gps?region:null}
         showsCompass={true}
-        minZoomLevel={6}
-        maxZoomLevel={9}
+        minZoomLevel={2}
+        maxZoomLevel={4}
        onRegionChange={(region)=> this.onRegionChange(region)}
    
-      ><UrlTile urlTemplate="https://raw.githubusercontent.com/gbanm/ExoPlanetService/master/ExoPlanetHunter.Web/Content/tiles/{z}/tile.png"  />
+      ><UrlTile urlTemplate="https://raw.githubusercontent.com/gbanm/ExoPlanetService/master/ExoPlanetHunter.Web/Content/tiles/4/tile.jpg"  />
         {PlanetList.filter(p=>p.Esi>=0.7 && p.Coordinate!==undefined).map((planet,index) =>  (
     <Marker
   
