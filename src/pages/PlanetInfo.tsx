a84cf43d123fd97568d3223e3a40ba29c5e1f8d1
gbanm <gbanm06@gmail.com>
2018-06-05 23:51:48 +0200

    svg image on info page

--- src/pages/PlanetInfo.tsx
+++ src/pages/PlanetInfo.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
-import {AppRegistry, StyleSheet, View } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text,Thumbnail,  } from 'native-base';
+import {AppRegistry, StyleSheet, View,ScrollView  } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text,Thumbnail, } from 'native-base';
 import{resource} from '../config/Resource'
 import{Planet} from '../service/getPlanets'
 import styles from '../styles/defaultStyle'
+
+
+import Svg,{
+  Circle,
+
+  ClipPath,
+  Path,
+  Image,
+ 
+  Use,
+  Defs,
+
+} from 'react-native-svg';
 interface PlanetProps{navigation:any}
 interface  PlanetPropsState { }
 export default class PlanetInfo extends React.Component<PlanetProps, PlanetPropsState> {
@@ -17,42 +30,55 @@ export default class PlanetInfo extends React.Component<PlanetProps, PlanetProps
 
 
     return (
-      <Container  style={styles.darkcontainer}>
-       
+      <View style={{ flex:1}}>
+      <ScrollView>   
+           <Container style={styles.darkcontainer}>
         <Content  style={styles.infoContent}>
-        <Content >
-               <Left>
-                <Thumbnail  source={planet.Img}/>
-              </Left>
-              <Body>
-              <Content >
-                
+      <Svg
+    height="300" 
+    width="300" 
+>
+    <Defs>
+        <ClipPath id="clip">
+            <Circle cx="150" cy="150" r="120" fill="#FFFFFF" />
+        </ClipPath>
+    </Defs>
+
+    <Image
+     
+     width="300" height="300" 
+        href={planet.Img} 
+        clipPath="url(#clip)"
+    />
+   
+</Svg>
+
+             
+         
               <Text style={styles.listText}></Text>
               <Text style={styles.listText}>{`${resource.planetname[0]}  ${planet.Name} ${resource.planetname[1]}  ${planet.Star.Name}  ${resource.planetname[2]} ${resource.const[planet.Star.Constellation-1]}` } {`${resource.decFormatdist[0]}${planet.Distance} ${resource.decFormatdist[1]} ` }</Text>
-             
-                   </Content>
+            
                    <Text style={styles.listText}> </Text>
-                   <Content >
+            
                  <Text style={styles.listText}>{resource.massInfo[planet.Type]} </Text>
                  <Text style={styles.listText}>{resource.compInfo[planet.Comp]} </Text>
-                 </Content>
+         
                  <Text style={styles.listText}> </Text>
                  <Text style={styles.listText}> </Text>
-              </Body>
-              <Right>
+           
                 <Text style={styles.listText}>{`${resource.orbit[0]} ${planet.Period} ${resource.orbit[1]} ${resource.decMean[0]} ${planet.MeanDistance} ${resource.decMean[1]}`}</Text>
        
                <Text style={styles.listText}></Text>
                <Text style={styles.listText}>{`${resource.hzd[planet.Hzd]} ${resource.hza[planet.Hza]} ${resource.atmosinfo[planet.Atmosphere]}`}</Text>
-
+               <Text style={styles.listText}></Text>
+                      <Text style={styles.listText}></Text>  
+                      <Text style={styles.listText}></Text>  
                       <Text style={styles.listText}></Text>
-                      <Text style={styles.listText}>{`${resource.meantemp[0]}  ${planet.Temp}. ${resource.meantemp[1]} ${planet.Name} ${resource.meantemp[2]} ${planet.TempMax} ${resource.meantemp[2]} ${planet.TempMin}`}</Text>
-              </Right>
-           
-        </Content>
-        </Content>
-        
-      </Container>
+                    
+                      </Content >
+        </Container></ScrollView>
+                      </View>
+ 
 
     );
   }
